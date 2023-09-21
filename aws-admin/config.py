18800bd03a3183b5e5de96e8ac2b2f64278b988c
yanzixiang2 <143473755+yanzixiang2@users.noreply.github.com>
2023-09-21 17:52:52 +0800

    Update config.py

--- aws-admin/config.py
+++ aws-admin/config.py
@@ -1,7 +1,7 @@
-customhost = "student.cvpusjsxfpmz.us-east-1.rds.amazonaws.com"
+customhost = "admin.cvpusjsxfpmz.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "student"
+customdb = "Admin"
 custombucket = "yanzixiang-student"
 customregion = "us-east-1a"
 
